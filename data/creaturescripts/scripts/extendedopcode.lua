5e47201df5525ba050e275fe91f864ec356c558e
Leonidas <leonidas@nc7i.com.br>
2025-04-16 12:53:29 -0300

    Refactor: Adicionado Ditto Memory e Adicionado Exibição Pokebar ao Logar

--- data/creaturescripts/scripts/extendedopcode.lua
+++ data/creaturescripts/scripts/extendedopcode.lua
@@ -15,15 +15,6 @@ function onExtendedOpcode(player, opcode, buffer)
 		player:handleTasksKill(buffer)
 		return
 	end
-
-   	if opcode == OPCODE_LANGUAGE then
-		-- otclient language
-		if buffer == 'en' or buffer == 'pt' then
-			-- example, setting player language, because otclient is multi-language...
-			--player:setStorageValue(SOME_STORAGE_ID, SOME_VALUE)
-		end
-	elseif opcode == OPCODE_SKILLBAR then
-
-	end
+	
    return true
 end
